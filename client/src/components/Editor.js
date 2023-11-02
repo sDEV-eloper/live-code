29e9a37a12658df7138a047bf65b2f687793bfdd
sDEV-eloper <thisissurajdev@gmail.com>
2023-11-02 10:00:22 +0530

    removed server folder

--- client/src/components/Editor.js
+++ client/src/components/Editor.js
@@ -4,11 +4,9 @@ import Client from "./Client";
 import { ImCopy } from "react-icons/im";
 import { MdOutlineExitToApp } from "react-icons/md";
 import { BsCheckAll } from "react-icons/bs";
-import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
+import MainEditor from "./MainEditor";
 import { initSocket } from "../socket";
 import ACTIONS from "../Action";
-
-
 import {
   Navigate,
   useLocation,
@@ -16,7 +14,6 @@ import {
   useParams,
 } from "react-router-dom";
 import toast from "react-hot-toast";
-import MainEditor from "./MainEditor";
 
 const Editor = () => {
   const [isCopied, setIsCopied] = useState(false);
@@ -27,12 +24,6 @@ const Editor = () => {
   const [clients, setClients] = useState([]);
   const socketRef = useRef(null);
   const codeRef = useRef(null);
-
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
 
   useEffect(() => {
     const init = async () => {
@@ -101,97 +92,69 @@ const Editor = () => {
   if (!location.state) {
     return <Navigate to="/" />;
   }
-
-  const screenWidth = window.innerWidth;
- 
-  console.log(screenWidth)
   return (
-    <div className="bg-[url('/src/utils/bg.jpg')]">
-  <button onClick={toggleSidebar} className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-200 rounded-lg sm:hidden focus:outline-none focus:ring-2">
-    <span className="sr-only">Open sidebar</span>
-   { !isSidebarOpen ? <AiFillCaretDown/> : <AiFillCaretUp/>}
-  </button>
-  <aside className="border border-red-800 fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0">
-      <div className="h-full px-3 py-4 bg-gray-800 flex justify-between flex-col">
-        <div>
-          <div className="flex justify-center border-b-2 border-gray-600">
-            <img src={Logo} alt="Logo" className="w-36 my-2" />
-          </div>
-          <ul className="space-y-2 font-medium mt-6 mb-2">
-            {clients.map((item) => {
-              return <Client key={item.socketId} username={item.username} />;
-            })}
-          </ul>
-        </div>
+    <div className="bg-[url('/src/utils/bg.jpg')] ">
+      <button className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-200 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 ">
+        <span className="sr-only">Open sidebar</span>
+        <svg
+          className="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clipRule="evenodd"
+            fillRule="evenodd"
+            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+          />
+        </svg>
+      </button>
 
-        <div className="flex text-white flex-col">
-          <button
-            onClick={copyRoomId}
-            className="flex justify-between items-center text-black cursor-pointer bg-[#ffffff] border-0 py-2 px-6 focus:outline-none rounded text-lg mb-2 active:scale-95 duration-150"
-          >
-            Copy Room ID
-            {!isCopied ? <ImCopy /> : <BsCheckAll />}
-          </button>
-          <button
-            onClick={() => reactNavigator("/")}
-            className="flex justify-center gap-3 items-center text-white cursor-pointer bg-[#ff3c3c] border-0 py-2 px-6 focus:outline-none rounded text-lg mb-2 active:scale-95 duration-150"
-          >
-            Exit
-            <MdOutlineExitToApp />
-          </button>
-        </div>
-      </div>
-    </aside>
- 
- {   !isSidebarOpen ? null :
-    <div className="text-white border">
-      <div className="h-full px-3 py-4 bg-gray-800 flex justify-between flex-col">
-        <div>
-          <div className="flex justify-center border-b-2 border-gray-600">
-            <img src={Logo} alt="Logo" className="w-36 my-2" />
+      <aside className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0">
+        <div className="h-full px-3 py-4 bg-gray-800 flex justify-between flex-col ">
+          <div>
+            <div className="flex justify-center   border-b-2 border-gray-600">
+              <img src={Logo} alt="Logo" className="w-36 my-2" />
+            </div>
+            <ul className="space-y-2 font-medium  mt-6">
+              {clients.map((item) => {
+                return <Client key={item.socketId} username={item.username} />;
+              })}
+            </ul>
           </div>
-          <ul className="space-y-2 font-medium mt-6">
-            {clients.map((item) => {
-              return <Client key={item.socketId} username={item.username} />;
-            })}
-          </ul>
-        </div>
 
-        <div className="flex text-white flex-col">
-          <button
-            onClick={copyRoomId}
-            className="flex justify-between items-center text-black cursor-pointer bg-[#ffffff] border-0 py-2 px-6 focus:outline-none rounded text-lg mb-2 active:scale-95 duration-150"
-          >
-            Copy Room ID
-            {!isCopied ? <ImCopy /> : <BsCheckAll />}
-          </button>
-          <button
-            onClick={() => reactNavigator("/")}
-            className="flex justify-center gap-3 items-center text-white cursor-pointer bg-[#ff3c3c] border-0 py-2 px-6 focus:outline-none rounded text-lg mb-2 active:scale-95 duration-150"
-          >
-            Exit
-            <MdOutlineExitToApp />
-          </button>
+          <div className="flex text-white flex-col ">
+            <button
+              onClick={copyRoomId}
+              className="flex justify-between items-center text-black cursor-pointer bg-[#ffffff] border-0 py-2 px-6 focus:outline-none rounded text-lg mb-2 active:scale-95 duration-150 "
+            >
+              Copy Room ID
+              {!isCopied ? <ImCopy /> : <BsCheckAll />}
+            </button>
+            <button
+              onClick={() => reactNavigator("/")}
+              className=" flex justify-center gap-3 items-center text-white cursor-pointer bg-[#ff3c3c] border-0 py-2 px-6 focus:outline-none rounded text-lg mb-2 active:scale-95 duration-150 "
+            >
+              Exit
+              <MdOutlineExitToApp />
+            </button>
+          </div>
+        </div>
+      </aside>
+
+      <div className="p-4 ml-64 h-screen ">
+        <div>
+          <MainEditor
+           socketRef={socketRef}
+           roomId={roomId}
+           onCodeChange={(code) => {
+               codeRef.current = code;
+           }}
+          />
         </div>
       </div>
     </div>
-    
-    }
-  
-
-  <div className={`p-4 md:ml-64 h-screen`}>
-    <div>
-      <MainEditor
-        socketRef={socketRef}
-        roomId={roomId}
-        onCodeChange={(code) => {
-          codeRef.current = code;
-        }}
-      />
-    </div>
-  </div>
-</div>
-
   );
 };
 
